919d264e974af8119ec5fe4210e0ec9c9617071b
denysetan <54625232+denysetan@users.noreply.github.com>
2021-04-07 01:03:49 +0800

    Update User Interface

--- app.py
+++ app.py
@@ -11,31 +11,36 @@ nlp = spacy.load('en_core_web_sm')
 import os
 import json
 import requests
-#from PIL import Image
 
 
 def main():
 	"""A Simple NLP app with Spacy-Streamlit"""
 
-	st.title("Natural Language Processing Dashboard")
-    # our_image = Image.open(os.path.join('SpaCy_logo.svg.png'))
-	# st.image(our_image)
+	st.title("Named-Entity Recognition for SG Locations")
 
 	menu = ["Home","NER"]
 	st.sidebar.subheader('Select the functions to display')
 	choice = st.sidebar.selectbox("Menu",menu)
-	
 
 	if choice == "Home":
-		st.header("Tokenization")
-		raw_text = st.text_area("Your Text","Enter Text Here")
-		docx = nlp(raw_text)
-		if st.button("Tokenize"):
-			spacy_streamlit.visualize_tokens(docx,attrs=['text','pos_','dep_','ent_type_'])
+		st.image('cover_photo.jpg')
+		st.header("Project Background")
+		''' Often, when a feedback comes in to URA, it would include references to place names, roads, addresses, etc. 
+		These references to locations would be **useful to planners** reviewing each case, as it provides a **spatial context**.
+ 		'''
+		st.image('image1.png')
+		st.header("Value of Project")
+		'''Previously, identifying the locations tagged to each text message and generating geospatial coordinates were **manual and time consuming**.
+		\nThe project therefore aims to utilise **Natural Language Processing Techniques** to automate the process, providing time savings for planners.
+		\n**The model has 2 parts:**
+		'''
+		st.image('image2.png')
 
 	elif choice == "NER":
-		st.subheader("Named Entity Recognition and GeoCoding")
-		raw_text = st.text_area("Your Text","Enter Text Here")
+		st.header("Named Entity Recognition and GeoCoding")
+		raw_text = st.text_area("","Enter Text Here")
+		
+		# perform NER and visualise output
 		docx = nlp(raw_text)
 		spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
 
@@ -45,30 +50,29 @@ def main():
 				locations.append(ent.text)
 
 		if locations:
-
 			# get geocoordinates from onemap API
 			query_address = locations[0]
 			query_string='https://developers.onemap.sg/commonapi/search?searchVal='+str(query_address)+'&returnGeom=Y&getAddrDetails=Y'
 			resp = requests.get(query_string)
-			#Convert JSON into Python Object 
-			data = json.loads(resp.content)
+			data = json.loads(resp.content) #Convert JSON into Python Object 
 
 			location_coordinates = []
 			location_names = []
-			length = data['found']
+			length = data['found'] # number of locations detected by onemap
 
 			maximum = 5
 			if length > 0 and length < 5:
 				maximum	= length
 			if length > 0:
+				# store location infos - names and coordinates
 				for i in range(maximum):
 					results = data['results'][i]
 					location_coordinates.append([results['LATITUDE'],results['LONGITUDE']])
 					location_names.append(results['SEARCHVAL'])
 
+				st.header("GeoCoded Map")
 				m = folium.Map(location=location_coordinates[0], zoom_start=16)
-
-				# add marker
+				# add markers
 				for point in range(0, maximum):
 					folium.Marker(location_coordinates[point], popup=location_names[point]).add_to(m)
 				# call to render Folium map in Streamlit
